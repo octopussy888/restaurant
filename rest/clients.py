21aec57842611d43dc032a54e78b662f39986bac
octopussy888 <ss.talia06@gmail.com>
2022-10-12 19:38:39 +0300

    Classes created

--- rest/clients.py
+++ rest/clients.py
@@ -3,8 +3,6 @@
 # so that when they make an order they don`t need to input their contacts each time.
 # It can be only one unique account for each telephone number AND e-mail.
 
-from HelloForm import HelloApp
-
 class Client:
     def __init__(self):
         self.c_id = 0000000000
@@ -15,12 +13,9 @@ class Client:
         self.c_address = None
         self.c_mail = None
         self.c_password = ''
+        self.role = 'User'
         self.delivery_arrived = False
 
     def register_client(self):
-        hello_app = HelloApp()
-        hello_app.run()
-        self.c_id += 1
-        self.c_name, self.c_sname, self.c_phone, self.c_address, self.c_mail, self.c_password = hello_app
-        self.fullname = f'{self.c_name} {self.c_sname}'
+        pass
 
