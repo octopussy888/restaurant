4ceea9fc907f0b3e3a3c78aa062e15b9325f65f6
octopussy888 <ss.talia06@gmail.com>
2022-10-26 14:55:46 +0300

    MVVM

--- rest_MVVM/View/SigningForms.py
+++ rest_MVVM/View/SigningForms.py
@@ -2,20 +2,25 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty
-from kivy. uix.screenmanager import Screen, ScreenManager
+from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.core.window import Window
-from clients import Client
+
+
+from Cross_platform.rest_MVVM.Model.clients import Client
 import os
 import sqlite3 as sql
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+BASE_DIR = r'C:\Users\Admin\PycharmProjects1\Cross_platform\rest_MVVM\Model'
 db_name = "restaurantProjectDB.db"
 db_path = os.path.join(BASE_DIR, db_name)
-conn = sql.connect(db_name, check_same_thread=False)
+print(db_path)
+conn = sql.connect(db_path, check_same_thread=False)
 cursor = conn.cursor()
 
 Window.size = (500, 700)
 
 client = Client()
+
 
 class HelloWindow(Screen):
     role = ObjectProperty(None)
@@ -47,6 +52,8 @@ class SignUpWindow(Screen):
                 or address == '' or mail == '' or password == '':
             print('You did not finish! ')
         else:
+            cursor.execute(''' SELECT MAX(IDClient) FROM Clients ''')
+            client.c_id = int(cursor.fetchall()[0][0])
             client.c_id += 1
             client.fullname = f'{first_name} {last_name}'
             client.c_phone = phone
@@ -57,8 +64,7 @@ class SignUpWindow(Screen):
                                              ({client.c_id}, '{client.fullname}', '{client.c_phone}', '{client.c_address}',
                                               '{client.c_mail}', '{client.c_password}', '{client.role}') """)
             conn.commit()
-            print(f"Hello, {client.c_name}!")
-            print(f'Hello, {first_name}!\n'
+            print(f'Hello, {client.fullname}!\n'
                   f'Thanks for signing up :)')
             exit()
 
